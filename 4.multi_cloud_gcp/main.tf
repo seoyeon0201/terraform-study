251d705fd99a335aaf85b951bf6216d9b5e54cba
seoyeon0201 <psy0201123@gmail.com>
2024-10-07 17:46:41 +0900

    feat: create gcp compute engine

---
+++ 4.multi_cloud_gcp/main.tf
@@ -0,0 +1,25 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = "${file("gcp-credentials.json")}"
+ project     = "terraform-gcp-437815"
+ region      = "us-central1"
+}
+
+// Create a GCE instance
+resource "google_compute_instance" "default" {
+  name         = "terraform"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
