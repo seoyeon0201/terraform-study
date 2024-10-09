77a76363ee0a2fd07be580dfeb9dc5b2d638fc2c
seoyeon0201 <psy0201123@gmail.com>
2024-10-09 17:54:16 +0900

    fix: add terraform.dev.tfvars for creating ec2

--- 1.create_ec2/main.tf
+++ 1.create_ec2/main.tf
@@ -1,12 +1,12 @@
 # main.tf
 
-terraform {
-	required_version = ">= 1.5.7"
-	
-	required_providers {
-		aws = {
-			source = "hashicorp/aws"
-			version = "~> 5.0" #틸드 연산자. 5.0.x 버전 설치
-		}
-	}
-}
+#terraform {
+#	required_version = ">= 1.5.7"
+#	
+#	required_providers {
+#		aws = {
+#			source = "hashicorp/aws"
+#			version = "~> 5.0" #틸드 연산자. 5.0.x 버전 설치
+#		}
+#	}
+#}
--- 1.create_ec2/provider.tf
+++ 1.create_ec2/provider.tf
@@ -1,5 +1,14 @@
 # provider.tf
 
+terraform {
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "~> 5.0"
+		}
+	}
+}
+
 provider "aws" {
 	profile = "terraform_prof" #aws configure --profile에서 설정한 profile
 	region = "ap-northeast-2"
