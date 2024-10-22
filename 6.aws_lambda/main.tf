c6c29ed2ac5ad964c4d86848c5cf0c78ce7a0b10
seoyeon0201 <psy0201123@gmail.com>
2024-10-22 15:46:23 +0900

    feat: add API Gateway

--- 6.aws_lambda/main.tf
+++ 6.aws_lambda/main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region = "ap-northeast-2"  
+  profile = "tf-seoyeon"
 }
 
 # Lambda 함수용 IAM 역할 생성
@@ -42,10 +43,48 @@ resource "aws_lambda_function" "my_lambda" {
   }
 }
 
-# Lambda 함수를 호출하기 위한 트리거 (선택 사항)
+# Lambda 함수를 호출하기 위한 트리거
 resource "aws_lambda_permission" "allow_invoke" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.my_lambda.function_name
   principal     = "apigateway.amazonaws.com"
+}
+
+# Lambda 함수 호출을 위한 API Gateway 설정
+resource "aws_api_gateway_rest_api" "my_api" {
+  name        = "MyAPI"
+  description = "API Gateway integrated with Lambda"
+}
+
+# API Gateway 리소스 생성 (/lambda)
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "lambda"  # /lambda 경로
+}
+
+# API Gateway 메서드 생성 (GET 메서드)
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# API Gateway와 Lambda 함수 연결 (통합)
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  integration_http_method = "POST"
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.my_lambda.invoke_arn
+}
+
+# API Gateway 배포
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name  = "dev"
 }
